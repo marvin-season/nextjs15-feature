e24c596e3f4b69285ac5cab68e8898675f217b9a
marvin <sea-son@foxmail.com>
2024-11-30 20:22:30 +0800

    fix: 127

--- src/lib/fetchTodos.ts
+++ src/lib/fetchTodos.ts
@@ -1,7 +1,7 @@
 import {Todo} from "@/types";
 
 export async function fetchTodos() {
-    const res = await fetch('http://localhost:2337/api/to-dos');
+    const res = await fetch('http://127.0.0.1:2337/api/to-dos');
     if (!res.ok) throw new Error('Failed to fetch todos');
 
     const data = await res.json();
@@ -10,7 +10,7 @@ export async function fetchTodos() {
 
 
 export async function fetchTodo(documentId: string) {
-    const res = await fetch(`http://localhost:2337/api/to-dos/${documentId}`)
+    const res = await fetch(`http://127.0.0.1:2337/api/to-dos/${documentId}`)
     if (!res.ok) throw new Error('Failed to fetch todos');
     const data = await res.json();
     return data.data;
